ce9de2b99bf7024ef1450bbc0fea2c401408b334
Lauvizh <webdev@loicfontaine.net>
2016-09-26 02:28:10 +0200

    URL simplifié pour la pagination

--- src/LF/EventBundle/Controller/DefaultController.php
+++ src/LF/EventBundle/Controller/DefaultController.php
@@ -33,7 +33,7 @@ class DefaultController extends Controller
         return $this->render('LFEventBundle:Default:theme.html.twig',array('theme'=>$theme));
     }
 
-    public function archiveAction(Request $request)
+    public function archiveAction($page, Request $request)
     {
 
     	$em = $this->getDoctrine()->getManager();
@@ -43,9 +43,12 @@ class DefaultController extends Controller
         $paginator  = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $query, /* query NOT result */
-            $request->query->getInt('page', 1)/*page number*/,
+            $request->query->getInt('page', $page)/*page number*/,
             10/*limit per page*/
+
         );
+        
+        $pagination->setUsedRoute('lf_event_archive');
 
         return $this->render('LFEventBundle:Default:archive.html.twig', array('pagination'=>$pagination));
     }
