984198512f567992b0c25a60ed0823be47c69dcd
Lauvizh <webdev@loicfontaine.net>
2016-09-26 22:05:58 +0200

    tentative personnalisation FOSUser Form

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new LF\FaceBundle\LFFaceBundle(),
             new LF\MemoBoxBundle\LFMemoBoxBundle(),
             new LF\UserBundle\LFUserBundle(),
+            new LF\AdminBundle\LFAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
